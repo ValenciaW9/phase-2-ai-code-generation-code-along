691e7e02a24aaafa68d95ee8376b5ed026e241db
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-12-23 19:04:54 -0500

    ai-code-generation-code-along/upload
    -ai-code-generation-code-along/

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+/** returns a list of books with the given generateKey.
+* @param {string} genre - The genre of books required.
+*@returns {Book[]} An array of Book objects matching the specified genre.
+*/
+function filterByGenre(genre, books) { }
+
+    var returnValue = {
+        //Die nächste Aktion
+        nextStep: "showText",
+        //Daten für die nächste Aktion
+        text: "Hello World"
+    }
+    //Daten zurückgeben
+    return JSON.stringify(returnValue);
---
+++ jsdoc index.js
@@ -0,0 +1,6 @@
+/** A book object repesentation.
+ * @ptypedef {Object} Book
+ * @property {string} title -The title of the book.
+ * @property {string} author - The author of the book.
+ * @property {string} genre - The genre of the book.
+ */
